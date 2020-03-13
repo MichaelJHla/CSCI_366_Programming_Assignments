400690a6c7fba2c03c8c3a1f8bc8af5659e5be75
MichaelJHla <57339492+MichaelJHla@users.noreply.github.com>
2020-03-12 19:57:30 -0600

    Simplify result available

--- src/Client.cpp
+++ src/Client.cpp
@@ -58,8 +58,7 @@ void Client::fire(unsigned int x, unsigned int y) {
 
 //Checks to see if a JSON with the results of a shot is available to be opened
 bool Client::result_available() {
-    ifstream resultFile;
-    resultFile.open("player_" + to_string(player) + ".result.json");
+    ifstream resultFile("player_" + to_string(player) + ".result.json");
     if (resultFile){
         return true;
     } else {
