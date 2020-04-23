4610a23651867e6e0d10b6d8627e1863828626d7
MichaelJHla <57339492+MichaelJHla@users.noreply.github.com>
2020-04-22 20:48:16 -0700

    Add functionality to BitArray2D.asm

--- src/BitArray2D.asm
+++ src/BitArray2D.asm
@@ -12,12 +12,33 @@ set_bit_elem:
         ; rdx contains row
         ; rcx contains col
 
-        ; add your code here
+        ; Index
+        imul rdx, rsi ; rdx = rdx * rsi (row = row*row_width)
+        add rdx, rcx ; rdx represents index. rdx = rdx + rcx (row = row + column)
+
+        ; Byte offset
+        mov rbx, rdx ; rbx = rdx, rbx is assigned the index value
+        shr rbx, 3 ; rbx represents Byte offset. rbx is shifted right 3 (rbx/8)
+
+        ; Bit offset
+        mov rsi, rbx ; rsi = rbx, rsi is assigned the Byte offset
+        mov rcx, rdx ; rcx = rdx, rcx is assigned the index value
+        shl rsi, 3 ; the Byte offset is shifted left 3 (rsi*8)
+        sub rcx, rsi ; rcx represents Bit offset. rcx = rcx - rsi (index = index - (ByteOffset*8))
+
+        ; Mask
+        mov rax, 7 ; assigns 7 to the rax register
+        mov rsi, 1 ; assigns 1 to the rsi register
+        sub rax, rcx ; rax = rax - rcx. This gets the proper position of the bit
+        mov rcx, rax ; moves the position to rcx register
+        shl rsi, cl ; rsi represents the mask. rsi is shifted left by the byteOffset value to the correct position
+
+        mov rax, rdi
+        or [rax], rsi
 
         mov rsp, rbp        ; restore stack pointer to before we pushed parameters onto the stack
         pop rbp             ; remove rbp from the stack to restore rsp to initial value
         ret                 ; return value in rax
-
 
 
 
@@ -31,7 +52,33 @@ get_bit_elem:
         ; rcx contains col
 
         ; add your code here - for now returning 0
-        mov rax, 0
+        ; mov rax, 0
+
+        ; Index
+        imul rdx, rsi ; rdx = rdx * rsi (row = row*row_width)
+        add rdx, rcx ; rdx represents index. rdx = rdx + rcx (row = row + column)
+
+        ; Byte offset
+        mov rbx, rdx ; rbx = rdx, rbx is assigned the index value
+        shr rbx, 3 ; rbx represents Byte offset. rbx is shifted right 3 (rbx/8)
+
+        ; Bit offset
+        mov rsi, rbx ; rsi = rbx, rsi is assigned the Byte offset
+        mov rcx, rdx ; rcx = rdx, rcx is assigned the index value
+        shl rsi, 3 ; the Byte offset is shifted left 3 (rsi*8)
+        sub rcx, rsi ; rcx represents Bit offset. rcx = rcx - rsi (index = index - (ByteOffset*8))
+
+        ; Mask
+        mov rax, 7 ; assigns 7 to the rax register
+        mov rsi, 1 ; assigns 1 to the rsi register
+        sub rax, rcx ; rax = rax - rcx. This gets the proper position of the bit
+        mov rcx, rax ; moves the position to rcx register
+        shl rsi, cl ; rsi represents the mask. rsi is shifted left by the byteOffset value to the correct position
+
+        mov rax, rdi
+        or [rax], rsi
+        setg al
+        movsx rax, al
 
         mov rsp, rbp        ; restore stack pointer to before we pushed parameters onto the stack
         pop rbp             ; remove rbp from the stack to restore rsp to initial value
